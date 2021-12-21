a2f7937b95941fb7711beb76fe0dacd5fc4798a4
David Flores <davidafloresvel@gmail.com>
2021-12-21 11:40:14 -0700

    ...

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function handleGetData(event){
     event.preventDefault();
 
     $.ajax({
-        url: `https://www.superheroapi.com/api.php/10157992628413204/${$input.val()}`,
+        url: `https://www.superheroapi.com/api.php/10157992628413204/search/${$input.val()}`,
     }).then(
       function(data){
        console.log(data);
